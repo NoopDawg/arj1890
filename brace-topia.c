406391b7ef1212c4acd17ebb4fcf0f1747b1dcd2
Anup R Jonchhe <arj1890@cs.rit.edu>
2017-10-13 00:16:45 -0400

    Initialized board

--- brace-topia.c
+++ brace-topia.c
@@ -1,8 +1,7 @@
+#define _BSD_SOURCE
 #include <stdlib.h>
-
 #include <unistd.h> //getopt
 #include <stdio.h> //fprintf
-#define _BSD_SOURCE
 
 
 
@@ -10,7 +9,7 @@ void printusage(){
 	fprintf (stderr,"usage: \nbrace_battle [-h] [-t N] [-c N] [-d dim] [-s s] [-v vac] [-e end] \n");
 }
 
-void getargs(int argc, char* argv[], int* cycmax, int* delay, int* dim, int* vac, int* str, int* end){
+void getargs(int argc, char* argv[], int* cycmax, int* delay, int* dim, double* vac, double* str, double* end){
 	
 	int c;
 	int tmp;  
@@ -61,7 +60,7 @@ void getargs(int argc, char* argv[], int* cycmax, int* delay, int* dim, int* vac
 				exit(1 + EXIT_FAILURE);
 			}
 		case 's':
-			tmp = (int)strtol(optarg, NULL, 10);
+			tmp = (double)strtol(optarg, NULL, 10);
 			if (tmp > 0 && tmp <100 ){
 				*str = tmp;
 				break;
@@ -71,7 +70,7 @@ void getargs(int argc, char* argv[], int* cycmax, int* delay, int* dim, int* vac
 				exit(1 + EXIT_FAILURE);
 			}
 		case 'v':
-			tmp = (int)strtol(optarg, NULL, 10);
+			tmp = (double)strtol(optarg, NULL, 10);
 			if (tmp > 0 && tmp <100 ){
 				*vac = tmp;
 				break;
@@ -81,7 +80,7 @@ void getargs(int argc, char* argv[], int* cycmax, int* delay, int* dim, int* vac
 				exit(1 + EXIT_FAILURE);
 			}
 		case 'e':
-			tmp = (int)strtol(optarg, NULL, 10);
+			tmp = (double)strtol(optarg, NULL, 10);
 			if (tmp > 0 && tmp <100 ){
 				*end = tmp;
 				break;
@@ -97,18 +96,73 @@ void getargs(int argc, char* argv[], int* cycmax, int* delay, int* dim, int* vac
 	}
 }
 
+
+void initsim(int dim, char room[dim][dim], double vac, double end){
+	int places = dim*dim;
+	int agentsn = (int) places -(places * (vac/100));
+	int endnum = (int) agentsn * (end/100);
+	int newnum = agentsn - endnum;
+
+	printf("places: %d, Agents: %d, Endlines: %d, Newlines: %d\n", places,
+		 agentsn, endnum, newnum);
+	int i = 0;
+	int r;
+	int c;
+	int pos;
+
+	for (int k = 0; k < dim; k++){
+		for (int j = 0; j < dim; j++){
+			room[k][j] = '.';
+		}
+	}
+	while (i < endnum){
+		pos = random() % places;
+		r = pos / dim;
+		c = pos % dim;
+		if (room[r][c] == '.'){
+			room[r][c] = 'e';
+			i++;
+		}else{
+			continue;
+		}
+	}
+	i = 0;
+	while (i < newnum){
+		pos = random() % places;
+		r = pos / dim;
+		c = pos % dim;
+		if (room[r][c] == '.'){
+			room[r][c] = 'n';
+			i++;
+		}else{
+			continue;
+		}
+	}
+	for (int k = 0; k < dim; k++){
+		for (int j = 0; j < dim; j++){
+			printf("%c", room[k][j]);
+		}
+		printf("\n");
+	}
+}
+
+
 int main(int argc, char* argv[]){
 
 	int cycmax = -1;
 	int delay = 900000;
 	int dim = 15;
-	int vac = 20;
-	int str = 50;
-	int end = 60;
+	double vac = 20;
+	double str = 50;
+	double end = 60;
 
 	getargs(argc, argv, &cycmax, &delay, &dim, &vac, &str, &end);
-	printf("The value are parameters are:\n delay: %d\n dim: %d\n str: %d\n"
-		" vac: %d\n end: %d\n ",delay, dim, str, vac, end);
+		printf("The value are parameters are:\n delay: %d\n dim: %d\n str: %.1f\n"
+		" vac: %.1f\n end: %.1f\n ",delay, dim, str, vac, end);
+	
+	char room[dim][dim];
+	srandom(41);
+	initsim(dim, room, vac, end);
 }
 
 
